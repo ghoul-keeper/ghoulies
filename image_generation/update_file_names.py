159b2d82db104ac6a901bba64e5ca5ee1b3850d2
Ghoul Keeper <defihoss@protonmail.com>
2021-10-23 14:33:13 -0600

    Fix update metadata script

--- image_generation/update_file_names.py
+++ image_generation/update_file_names.py
@@ -1,7 +1,8 @@
 import os
 import json
 
-counter = 0
+counter_png = 0
+counter_json = 0
 
 directory = "<path_to_directory>"
 
@@ -15,6 +16,25 @@ def listdir_nohidden(path):
 list_of_files = sorted(listdir_nohidden(directory),
                        key=lambda x: int(os.path.splitext(x)[0]))
 
+print("Updating images....")
+
+for idx, file in enumerate(list_of_files):
+    is_png_file = os.path.splitext(file)[1] == ".png"
+
+    if(file == ".DS_Store"):
+        break
+
+    if(is_png_file):
+        f_new = str(counter_png) + '.png'
+        os.rename(os.path.join(directory, file),
+                  os.path.join(directory, f_new))
+        counter_png += 1
+
+        print("Old png file " + file + " New png file: " + f_new)
+
+
+print("Finished updating pngs...")
+print("Updating json files....")
 
 for idx, file in enumerate(list_of_files):
     is_json_file = os.path.splitext(file)[1] == ".json"
@@ -23,15 +43,10 @@ for idx, file in enumerate(list_of_files):
         break
 
     if(is_json_file):
-        f_new = str(counter) + '.json'
-    else:
-        f_new = str(counter) + '.png'
-        counter += 1
+        f_new = str(counter_json) + '.json'
+        counter_json += 1
 
-    os.rename(os.path.join(directory, file), os.path.join(directory, f_new))
-
-    if(is_json_file):
-        with open(directory + f_new, "r") as jsonFile:
+        with open(directory + file, "r") as jsonFile:
             data = json.load(jsonFile)
 
         data["image"] = os.path.splitext(f_new)[0] + '.png'
@@ -41,4 +56,9 @@ for idx, file in enumerate(list_of_files):
         with open(directory + f_new, "w") as jsonFile:
             json.dump(data, jsonFile, indent=4)
 
-    print("Old file " + file + " New file: " + f_new)
+        os.remove(os.path.join(directory, file))
+
+        print("Old json file " + file + " New json file: " + f_new)
+
+
+print("All done!")
